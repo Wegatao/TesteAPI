c9dd18f6a1aea456e07256a39e3eaa0ff6131ffd
Welber_Willian <102258270+Wegatao@users.noreply.github.com>
2025-04-11 18:48:43 -0300

    Update app.py
    fiz mais umas mudanças pra que que ele venha ter reposta de requisição.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)  # <-- Habilita CORS
 
 @app.route("/")
 def home():
@@ -10,3 +12,4 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
